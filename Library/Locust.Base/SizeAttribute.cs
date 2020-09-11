e8dda1606c639e6b98387f2b1d04f31592115623
Mansoor Omrani <mansoor.omrani@gmail.com>
2020-09-11 13:05:13 +0430

    Created infrstructure; Added Locust.Base

---
+++ Library/Locust.Base/SizeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locust.Base
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class SizeAttribute : Attribute
+    {
+        public int? Value { get; set; }
+        public SizeAttribute(int value)
+        {
+            Value = value;
+        }
+        public SizeAttribute(string value)
+        {
+            this.Value = string.Compare(value, "max", StringComparison.OrdinalIgnoreCase) == 0 ? -1: System.Convert.ToInt32(value);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Locust.NET
+Locust.NET is a multi-purpose package containing more than a hundred utility and helper libraries.
+
+It is a programming layer over FCL (.NET class libraries) but below .NET projects.
+
+It can be used in various projects, mostly web applications.
