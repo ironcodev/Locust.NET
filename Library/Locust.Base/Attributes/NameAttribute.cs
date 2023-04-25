4f2430065f4ecd834af16209a884b34800e9a0a6
Mansoor Omrani <mansoor.omrani@gmail.com>
2023-04-26 03:02:03 +0430

    Added Name and Order attributes

---
+++ Library/Locust.Base/Attributes/NameAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Locust.Base
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class NameAttribute : Attribute
+    {
+        public string Value { get; set; }
+        public NameAttribute(string value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ Library/Locust.Base/Attributes/OrderAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Locust.Base
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class OrderAttribute : Attribute
+    {
+        public int Value { get; set; }
+        public OrderAttribute(int value)
+        {
+            Value = value;
+        }
+    }
+}
